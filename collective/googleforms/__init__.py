f1ba6776593633d3cd95c26ab1c482df23a038fc
JeanMichel FRANCOIS aka toutpt <toutpt@gmail.com>
2011-04-20 19:48:00 +0200

    add view and locales structure

--- collective/googleforms/__init__.py
+++ collective/googleforms/__init__.py
@@ -1,4 +1,7 @@
   # -*- extra stuff goes here -*- 
+from zope.i18nmessageid import MessageFactory
+
+googleformsMessageFactory = MessageFactory('collective.googleforms')
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
